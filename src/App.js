bc5b3b5b3e209734cd833b2c269d7c3991750290
levintoo <tooklevin@gmail.com>
2022-06-30 02:11:33 +0300

    components,fetching api,local storage, mapping array to ui

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-        <h1>
-        Hello from React
-        </h1>
+        <h1>Hello from React</h1>
         <Pages />
     </div>
   );
